6e113e0d8b3e724ab05e2e8dc433c37fa6190fbc
enzodon <enzomartindon@gmail.com>
2022-08-03 15:01:18 -0300

    Correcciones finales paginas
    Se corrigieron estilos de pagina Guaraní y carreras

--- pages/carreras/src/carreras.js
+++ pages/carreras/src/carreras.js
@@ -84,13 +84,21 @@ function mostrar_menu_lateral(){
         document.getElementById("menu_div_smartphone").style.display="none";
         menu_lateral = false;
         document.getElementById("boton_menu_smartphone").innerHTML = `   
-        Menú`;
-
+        <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg> `;
+        document.getElementById("contenido").style.height = "100%";
+        document.getElementById("contenido").style.overflow = "hidden";
     }else{
         document.getElementById("menu_div_smartphone").style.display="flex";
         menu_lateral = true;
         document.getElementById("boton_menu_smartphone").innerHTML = `   
-        Ocultar`;
+        <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        </svg>`;
+    document.getElementById("contenido").style.height = "100vh";
+        document.getElementById("contenido").style.overflow = "hidden";
     }
 }
 
@@ -169,7 +177,9 @@ function incluir_funcion_botones(){
             document.getElementById("menu_div_smartphone").style.display="none";
             menu_lateral = false;
             document.getElementById("boton_menu_smartphone").innerHTML = `   
-            Menu`;
+            <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>`;
         }
     };
 }
