8d652954530fae27cc70983b17f8ebe613f0a654
enzodon <enzomartindon@gmail.com>
2022-08-07 00:46:18 -0300

    Primera versión pagina Home terminada

---
+++ pages/home/src/home.js
@@ -0,0 +1,51 @@
+let  menu_lateral = false;
+
+function mostrar_menu_lateral(){
+    if(menu_lateral){
+        document.getElementById("menu_div_smartphone").style.display="none";
+        menu_lateral = false;
+        document.getElementById("boton_menu_smartphone").innerHTML = `   
+        <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg> `;
+        document.getElementById("main").style.height = "100%";
+        document.getElementById("main").style.overflow = "hidden";
+    }else{
+        document.getElementById("menu_div_smartphone").style.display="flex";
+        menu_lateral = true;
+        document.getElementById("boton_menu_smartphone").innerHTML = `   
+        <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        </svg>`;
+    document.getElementById("main").style.height = "100vh";
+        document.getElementById("main").style.overflow = "hidden";
+    }
+}
+
+function incluir_funcion_botones(){
+    document.getElementById("boton_menu_smartphone").onclick = function() {
+        mostrar_menu_lateral();
+    }
+
+    window.onresize = function(){
+        var anchoVentana = window.innerWidth;
+        if(anchoVentana >= 886){
+            document.getElementById("menu_div_smartphone").style.display="none";
+            menu_lateral = false;
+            document.getElementById("boton_menu_smartphone").innerHTML = `   
+            <svg class="icono-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>`;
+        }
+    };
+}
+
+function change_html(enlace){
+    window.location.href = enlace;
+}
+
+
+function carga_inicial(){
+    incluir_funcion_botones();
+}
